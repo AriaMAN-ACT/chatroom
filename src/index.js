b7f2da8b4cd18a36a02d5aadb086e0daf9ea42eb
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-29 21:57:20 +0430

    simple chat room now

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const http = require('http');
 const socketIo = require('socket.io');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    console.log('server started');
-});
+const socketEvents = require('./model/SocketEvents');
 
-const socket = socketIo(server);
+const app = express();
 
-socket.on('connection', (socket, req) => {
-    socket.emit('successfulConnection', 'server connected');
+app.use(express.static('public'));
 
-    socket.on('message', msg => {
-        console.log(msg);
+const server = app.listen(3000);
+
+const io = socketIo(server);
+
+io.on('connection', (socket, req) => {
+    socket.emit(socketEvents.successfulConnection, 'server connected');
+
+    socket.on(socketEvents.clientMsg, msg => {
+        io.emit(socketEvents.serverMsg, {msg, id: socket.id});
     });
 });
-
-server.listen(3000);
---
+++ src/model/SocketEvents.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    successfulConnection: 'successfulConnection',
+    serverMsg: 'serverMsg',
+    clientMsg: 'clientMsg'
+});
